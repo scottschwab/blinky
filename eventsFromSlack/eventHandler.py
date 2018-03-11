d1eac636c538a966cb7714f4a0b554c2f3f4a0d6
Scott Schwab <scott.schwab@gmail.com>
2018-03-11 14:06:12 -0500

    after rework

--- eventsFromSlack/eventHandler.py
+++ eventsFromSlack/eventHandler.py
@@ -6,16 +6,13 @@ import boto3
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+known_channels = dict()
+known_channels['G02NE4N0M'] = "object_storage"
+known_channels['D33EMSV3J'] = "bradley schwab"
+
 bucket = 'slackstatus'
 obj = 'latestupdate'
 
-def write_msg_to_s3(message):
-    logging.info("writing message [" + message + "]")
-    s3 = boto3.resource('s3')
-    o2 = s3.Object(bucket, obj)
-    v = o2.put(Body=message, ContentLength=len(message), ContentType="text/plain")
-    logger.info(v)
-    return v['ResponseMetadata']['HTTPStatusCode']
 
 def handle_authorization(body_json):
     """ used on verify the end points works, just returning a message that contains the 
@@ -29,28 +26,70 @@ def handle_authorization(body_json):
     }
 
 
+def write_msg_to_s3(message):
+    """Write the message to a known s3 object"""
+    logging.info("writing message [" + message + "]")
+    s3 = boto3.resource('s3')
+    o2 = s3.Object(bucket, obj)
+    v = o2.put(Body=message, ContentLength=len(message), ContentType="text/plain")
+    logger.info(v)
+    return v['ResponseMetadata']['HTTPStatusCode']
+
+
+def determine_channel_tag(message_json):
+    if 'channel' in message_json:
+        channel_name = 'channel'
+    elif 'group' in message_json:
+        channel_name = 'group'
+    else:
+        channel_name = None
+    return channel_name
+
+
+def build_msg_from_message_body(message_json, channel, event_time):
+    msg = dict()
+    msg['channel'] = channel
+    msg['text'] = message_json['text']
+    msg['event_time'] = event_time
+    return json.dumps(msg)
+
+
 def handle_message(body_json):
-    known_channels = dict()
-    known_channels['G024BE91L'] = "fire_engine"
+    logger.info("handling a message")
+    status = 501
 
-    pattern = "off"
-    if body_json['channel'] in known_channels:
-        pattern = known_channels[body_json['channel']]
+    logger.info(body_json)
+    if 'event' in body_json:
+        message_json = body_json['event']
+        event_time = body_json['event_time']
+        channel_name = determine_channel_tag(message_json)
+        if channel_name is not None and message_json[channel_name] in known_channels:
+            status = write_msg_to_s3(build_msg_from_message_body(
+                message_json, 
+                known_channels[message_json[channel_name]],
+                event_time))
+    return { "statusCode":status  }
 
-    msg = '{ "pattern" : "' + pattern + '" }'
-    status = write_msg_to_s3(msg)
-    return {
-        "statusCode":status
-    }
+
+eventHandler = dict()
+eventHandler["url_verification"] = handle_authorization
+eventHandler["event_callback"] = handle_message
+
+def handle_type_top_level(body_json):
+    logger.info("in handle_type_top_level")
+    if body_json['type'] in eventHandler:
+        action = eventHandler[body_json['type']]
+        response = action(body_json) 
+        logger.info(response)
+        return response    
+    logger.info("nothing in handle_type_top_level")
+
+    
 
 def lambda_handler(event, context):
     """ main driver called from AWS """
 
-    eventHandler = dict()
-    eventHandler["url_verification"] = handle_authorization
-    eventHandler["message"] = handle_message
-
-    logger.warn('got event {}'.format(event))
+    logger.info('got event {}'.format(event))
     if event is None or 'body' not in event:
         logger.warn("no body in the request")
         return {
@@ -59,15 +98,14 @@ def lambda_handler(event, context):
             "body" : "missing body in request"
         }
 
-    logger.info("setting event to body")
+    logger.info("setting event sent in to body")
     body_json = json.loads(event['body'])
 
-
-    if body_json['type'] in eventHandler:
-        action = eventHandler[body_json['type']]
-        response = action(body_json)
-        return response
-
+    if 'type' in body_json:
+        logger.info("handing a type in the body")
+        return handle_type_top_level(body_json)
+    
+    logger.info("nothing triggered so falling through")
     return { 
         "headers" : { "Context-type" : "text/plain" },
         "statusCode" : 501,
--- eventsFromSlack/eventHandler_test.py
+++ eventsFromSlack/eventHandler_test.py
@@ -34,18 +34,52 @@ def test_body_validate():
     assert msg['body'] == '{ "challenge" : "{3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P}" }'
 
 
-def test_channel():
-    x = dict()
-    body = dict()
-    x["type"] = "message"
-    x["channel"] = "G024BE91L"
-    x["user"] = "U2147483697"
-    x["text"] = "hello world"
-    x["ts"]  = "1355517523.000005"
-    body['body'] = json.dumps(x)
-    msg = eventHandler.lambda_handler(body,None)
-    assert msg is not None
-    assert msg['statusCode'] == 200
+def test_determine_channel_tag():
+    msg1 = dict()
+    msg1['channel'] = 'foo'
+    assert 'channel' == eventHandler.determine_channel_tag(msg1)
+    msg2 = dict()
+    msg2['group'] = 'bar'
+    assert 'group' == eventHandler.determine_channel_tag(msg2)
+    msg3 = dict()
+    msg3['frog'] = 'bar'
+    assert eventHandler.determine_channel_tag(msg3) is None
 
 
+def test_build_msg_from_message_body():
+    message_body = dict()
+    message_body['text'] = 'hello world'
+    back_str = eventHandler.build_msg_from_message_body(message_body, 'dog',1)
+    back = json.loads(back_str)
+    assert back['channel'] == 'dog'
+    assert back['text'] == 'hello world'
+    assert back['event_time'] == 1
 
+
+def test_message():
+    msg_in = dict()
+    
+    msg_in["body"] = """
+    {
+        "token": "XXYYZZ",
+        "team_id": "TXXXXXXXX",
+        "api_app_id": "AXXXXXXXXX",
+        "event": {
+            "type": "message",
+            "channel": "D33EMSV3J",
+            "user": "U2147483697",
+            "text": "Hello world",
+            "ts": "135551523.000005"
+        },
+        "type": "event_callback",
+        "authed_users": [
+                "UXXXXXXX1",
+                "UXXXXXXX2"
+        ],
+        "event_id": "Ev08MFMKH6",
+        "event_time": 1234567890
+    }"""
+
+    
+    msg = eventHandler.lambda_handler(msg_in,None)
+    logger.info(msg)
